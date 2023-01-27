5dccd122171ceff2074bd3821c0dc20bf7f88e0c
lensbelete <120299182+lensbelete@users.noreply.github.com>
2023-01-27 23:51:09 +0300

    Add files via upload

---
+++ src_bootstrap/script.js
@@ -0,0 +1,20 @@
+function login() {
+  var uname = document.getElementById("email").value;
+  var uname = document.getElementById("pwd1").value;
+  if (uname == "") {
+    alert("please enter user name.");
+  } else if (pwd1 == "") {
+    alert("enter the password");
+  } else if (pwd1.length < 6 || pwd1.length > 6) {
+    alert("password min and max length is 6");
+  } else {
+    alert(
+      "thank you for login in and you are are redirecting to your profile page."
+    );
+    window.location.href = "yourProfile.html";
+  }
+}
+function clearFunc() {
+  document.getElementById("email").value = "";
+  document.getElementById("pwd1").value = "";
+}
